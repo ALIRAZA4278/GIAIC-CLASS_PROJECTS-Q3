c065221bc5e1f57ca0707b47684fd1808debbdac
Ali Raza <156216939+ALIRAZA4278@users.noreply.github.com>
2025-04-11 23:10:51 +0500

    project 4

---
+++ Project 4 Assignments/Assignments 00 to 05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program adds two numbers.")
+    num1: int = int(input("Enter first number: "))
+    num2: int = int(input("Enter second number: "))
+    total: int = num1 + num2
+    print("The total is " + str(total) + ".")
+    
+ 
+    
+if __name__ == '__main__':
+    main()
